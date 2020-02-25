632ba9af54e5d65c3def36232864c62172dbf02c
Brandon <brandonbass@u.boisestate.edu>
2020-02-24 20:00:41 -0700

    add dew fixed class1

---
+++ drewsave.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<?php
+require_once('nav.php');
+ ?>
+ <?php
+require_once('footer.php');
+  ?>
+<main>
+  <h1> Hello my name is Drew and I only wear orange north face coats.
+  </h1>
+  <p> Hello my nanme is Drew Barrymore and I'm from Hoth that is why I only wear
+     orange north face coats. At a yung age my parents had me fight black bears
+     with only my fists. This is how I trained to become the next hokage. Thanks
+     for comin to my ted talk.
+</p1>
+     <style>
+body{
+  background-color: #ffcce6
+}
+h1{
+  font-family: monospace;
+  letter-spacing: 40px;
+}
+p
+{
+  font-size: 38px;
+  letter-spacing: 10px;
+}
+  .shadowed
+  {
+    text-shadow: 2px 2px #99ccff;
+  }
+  </style>
+</main>
+</html>
