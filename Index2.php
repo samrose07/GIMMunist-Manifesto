8ca70cd773fc76c85a7a68499dcdb4f1d92b2ef9
Brandon <brandonbass@u.boisestate.edu>
2020-02-24 20:09:39 -0700

    index 2 for class exerisce

---
+++ Index2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php
+require_once('nav.php');
+ ?>
+ <?php
+require_once('footer.php');
+?>
+<main>
+<h1> This is a home page.</h1>
+<p> I hope this works boys>
+<p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi.
+  Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum.
+  Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor
+  velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec
+  commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut
+  felis. Sed vehicula pellentesque quam.</p>
