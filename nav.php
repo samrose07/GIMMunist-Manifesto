1cb94ccca37cd120375939102a8942e546a5a51b
samrose07 <60021547+samrose07@users.noreply.github.com>
2020-02-24 20:27:29 -0700

    sd

--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
     <ul class="nav navbar-nav">
-      <li class="active"><a href="/newPages/index2.php">Home</a></li>
+      <li class="active"><a href="/newPages/Index2.php">Home</a></li>
       <li><a href="/newPages/brandoBio.php">Brandon Bass</a></li>
       <li><a href="/newPages/drewsave.php">Drew Moran</a></li>
       <li><a href="/newPages/LifeOfSam.php">Samuel Rose</a></li>
