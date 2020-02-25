7fcbcfbeff9d7bce2d29fee5519d57da1e3560e5
samrose07 <60021547+samrose07@users.noreply.github.com>
2020-02-24 20:21:23 -0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,13 +1,13 @@
-<?php
-  $pages = scandir('newPages');
-
-  foreach($pages as $page)
-  {
-    if(in_array($page))
-    {
-      $link = 'newPages/' . $page;
-      $safeName = str_replace('.php','', $page);
-      echo'<a href="' . $link . '">' . $safeName . '</a> &nbsp;';
-    }
-  }
- ?>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
