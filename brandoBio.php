5a272ae41c6ba67d7e384f6669ecfa361dbb5380
samrose07 <60021547+samrose07@users.noreply.github.com>
2020-02-24 19:57:46 -0700

    Brandons page

---
+++ brandoBio.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<?php require_once('nav.php');
+?>
+<?php require_once('footer.php');
+?>
+<body>
+<h1> My name is Brandon Bass </h1>
+<p> My middle name is Bartholemew, and I was abandoned as a child. I spent my early years being bullied by other birds my age,
+  and yet I have found ways to be compassionate. I enjoy long walks on the beach and getting into Twitter beefs with Sam Rose.
+  Currently I am taking loads of steroids and hormone injections to keep my boyish figure. I love you. Thank you for coming
+  to my TedTalk. </p>
+</body>
+</html>
